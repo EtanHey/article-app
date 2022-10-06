fa3b479965cea7beb45b6061f8885012e95a7be3
Etan Joseph Heyman <etan@heyman.net>
2022-10-06 12:15:01 -0500

    no dist

--- server.ts
+++ server.ts
@@ -1,36 +1,30 @@
-import express from "express";
-import mongoose from "mongoose";
-import cookieParser from "cookie-parser";
-const MONGODB_URI = process.env.MONGODB_URI
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+const MONGODB_URI = process.env.MONGODB_URI;
 const app = express();
 const port = process.env.PORT || 4001;
 require('dotenv').config();
 
-
-app.use(express.static("public/build"));
-app.use(cookieParser())
+app.use(express.static('public/build'));
+app.use(cookieParser());
 app.use(express.json());
 
-
 mongoose
-  .connect(
-    MONGODB_URI
-  )
-  .then(() => {
-    console.log("connected to Mongoose");
-  })
-  .catch((err) => {
-    console.log("Failed to connect to Mongoose:");
-    console.log(err.message);
-  });
-
+    .connect(MONGODB_URI)
+    .then(() => {
+        console.log('connected to Mongoose');
+    })
+    .catch((err) => {
+        console.log('Failed to connect to Mongoose:');
+        console.log(err.message);
+    });
 
 import userRouter from './server/router/userRouter';
 import articleRouter from './server/router/articleRouter';
 app.use('/api/users', userRouter);
 app.use('/api/articles', articleRouter);
 
-
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+    return console.log(`Express is listening at http://localhost:${port}`);
 });
